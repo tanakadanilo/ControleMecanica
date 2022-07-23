5783fd189d2a1929103dbfb051d9d141569ee835
Danilo Tanaka <tanakadanilo867@gmail.com>
2022-07-22 21:51:15 -0300

    implementando controle de banco de dados de serviços

--- src/persistencia/ManipulaBancoServicos.java
+++ src/persistencia/ManipulaBancoServicos.java
@@ -4,6 +4,9 @@
  */
 package persistencia;
 
+import exceptions.DataBaseException;
+import exceptions.InvalidInputException;
+import exceptions.SystemErrorException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
@@ -13,60 +16,33 @@ import modelos.Servico;
  *
  * @author tanak
  */
-public class ManipulaBancoServicos implements IManipulaBanco<Servico> {
+public class ManipulaBancoServicos extends DataBase implements IManipulaBanco<Servico> {
+
+    public ManipulaBancoServicos() {
+        super(4, "Servicos.txt");
+    }
 
     @Override
-    public Servico parse(String dados) throws Exception {
-        String[] dadosServico = dados.split(";");
+    public Servico parse(String dados) throws SystemErrorException {
+        try {
+            String[] dadosServico = dados.split(";");
 // * id, nomeServico, valorMaoDeObra, cadastro está ativo
-        if (dadosServico.length != 4) {
-            throw new Exception("Dados incorretos");
-        }
-        Servico s = new Servico(dadosServico[1], Double.parseDouble(dadosServico[2]));
-        if (dadosServico[3].equals(String.valueOf(false))) {
-            s.setCadastroAtivo(false);
-        }
-        return s;
-    }
-
-    @Override
-    public String getNomeDoArquivoNoDisco() {
-        return Servico.getNomeArquivoDisco();
-    }
-
-    @Override
-    public int getID(Servico obj) throws Exception {
-        try ( BufferedReader br = new BufferedReader(new FileReader(Servico.getNomeArquivoDisco()))) {
-            String linha = br.readLine();
-            while (linha != null) {
-                Servico s = parse(linha);// * parsing linha
-                if (s.equals(obj) && s.isCadastroAtivo()) {//  * encontrou
-                    return Integer.parseInt(linha.split(";")[0]);// * retornando o id
-                }
-                linha = br.readLine();
+            if (dadosServico.length != this.getQuantidadeDeDadosSalvos()) {
+                throw new DataBaseException("os dados:\"" + dados + "\" estão incorretos");
             }
+            Servico s = new Servico(dadosServico[1], Double.parseDouble(dadosServico[2]));
+            if (dadosServico[3].equals(String.valueOf(false))) {
+                s.setCadastroAtivo(false);
+            }
+            return s;
+        } catch (DataBaseException e) {
+            corrigeBanco();
+            throw new IllegalStateException("Os dados \"" + dados + "\" foram corrompidos");//  * o método acima já soltou exceção quando encontrou a falha no banco de dados
         }
-        return 0;// * objeto não encontrado
     }
 
     @Override
-    public String getNomeArquivoID() {
-        return Servico.getArquivoID();
-    }
-
-    @Override
-    public boolean isCadastroAtivo(Servico obj) {
-        return obj.isCadastroAtivo();
-    }
-
-    @Override
-    public Servico setCadastroAtivo(Servico obj, boolean flag) {
-        obj.setCadastroAtivo(flag);
-        return obj;
-    }
-
-    @Override
-    public int buscar(String dado) throws Exception {
+    public int buscar(String dado) throws InvalidInputException, SystemErrorException {
         ArrayList<Servico> listaServicos = buscarTodos();
         for (Servico s : listaServicos) {
             if (s.getNomeServico().equals(dado)) {//  * encontrou
@@ -77,8 +53,8 @@ public class ManipulaBancoServicos implements IManipulaBanco<Servico> {
     }
 
     @Override
-    public boolean ativarEasterEgg(Servico obj) {
-        return obj.getNomeServico().toUpperCase().contains("das couve".toUpperCase());
+    public int getQuantidadeDeDadosSalvos() {
+        return this.quantidadeDeDadosNoBanco;
     }
 
 }
