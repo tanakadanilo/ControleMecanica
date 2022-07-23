cf7d95a1b5fa62f8cb5438fae98455b36a0a1ecb
Danilo Tanaka <tanakadanilo867@gmail.com>
2022-07-22 21:39:26 -0300

    implementando controle de banco de dados de cliente pessoa física

--- src/persistencia/ManipulaBancoPessoaFisica.java
+++ src/persistencia/ManipulaBancoPessoaFisica.java
@@ -6,8 +6,13 @@
 package persistencia;
 
 import enumerations.EstadosBrazil;
+import exceptions.DataBaseException;
+import exceptions.InvalidInputException;
+import exceptions.SystemErrorException;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -18,94 +23,67 @@ import modelos.auxiliares.Endereco;
  *
  * @author tanak
  */
-public class ManipulaBancoPessoaFisica implements IManipulaBanco<PessoaFisica> {
+public class ManipulaBancoPessoaFisica extends DataBase implements IManipulaBanco<PessoaFisica> {
+
+    public ManipulaBancoPessoaFisica() {
+        super(8, "ClientePessoaFisica.txt");
+    }
 
     @Override
-    public PessoaFisica parse(String dadosCompletos) throws Exception {
-        String[] dados = dadosCompletos.split(";");
+    public PessoaFisica parse(String dadosCompletos) throws SystemErrorException {
+        try {
+            String[] dados = dadosCompletos.split(";");
 //  * id, nome, cpf, data de nascimento (dd/MM/yyyy),
 //  * array de telefones, email, endereco, cadastro está ativo
-        if (dados.length != 8) {
-            System.out.println(dadosCompletos);
-            throw new Exception("Dados incorretos" + dadosCompletos);
-        }
-        String[] dadosEndereco = dados[6].split(",");
-        if (dadosEndereco.length != 8) {
-            throw new Exception("Dados incorretos, de endereço");
-
-        }
-        Endereco endereco = new Endereco(dadosEndereco[0],//    * tipo de logradouro
-                dadosEndereco[1],//    * logradouro
-                dadosEndereco[2],//    * numero
-                dadosEndereco[3],//    * complemento
-                dadosEndereco[4],//    * bairro
-                dadosEndereco[5],//    * cidade
-                Enum.valueOf(EstadosBrazil.class,
-                        dadosEndereco[6]),//    * estado, seguindo o Enum
-                dadosEndereco[7]);//    * cep
-
-        Date dataNascimento = new SimpleDateFormat("dd/MM/yyyy").parse(dados[3]);
-
-        String[] telefones = dados[4].substring(dados[4].indexOf("[") + 1, dados[4].lastIndexOf("]")).split(",");
-
-        PessoaFisica p = new PessoaFisica(dados[1],// * nome
-                dados[2],// * CPF
-                dataNascimento,// * data de nascimento
-                dados[5],// * email
-                endereco,// * endereco
-                telefones);
-
-        if (dados[7].equals(String.valueOf(false))) {
-            p.setCadastroAtivo(false);
-        }
-        return p;
-    }
-
-    @Override
-    public String getNomeDoArquivoNoDisco() {
-        return PessoaFisica.getNomeArquivoDisco();
-    }
-
-    @Override
-    public int getID(PessoaFisica obj) throws Exception {
-        try ( BufferedReader br = new BufferedReader(new FileReader(PessoaFisica.getNomeArquivoDisco()))) {
-            String linha = br.readLine();
-            while (linha != null) {
-                PessoaFisica v = parse(linha);// * parsing linha
-                if (v.equals(obj) && v.isCadastroAtivo()) {//  * encontrou
-                    return Integer.parseInt(linha.split(";")[0]);// * retornando o id
-                }
-                linha = br.readLine();
+            if (dados.length != 8) {
+                System.out.println(dadosCompletos);
+                throw new DataBaseException("Dados incorretos" + dadosCompletos);
             }
+            String[] dadosEndereco = dados[6].split(",");
+            if (dadosEndereco.length != 8) {
+                throw new DataBaseException("Dados incorretos, de endereço");
+
+            }
+            Endereco endereco = new Endereco(dadosEndereco[0],//    * tipo de logradouro
+                    dadosEndereco[1],//    * logradouro
+                    dadosEndereco[2],//    * numero
+                    dadosEndereco[3],//    * complemento
+                    dadosEndereco[4],//    * bairro
+                    dadosEndereco[5],//    * cidade
+                    Enum.valueOf(EstadosBrazil.class,
+                            dadosEndereco[6]),//    * estado, seguindo o Enum
+                    dadosEndereco[7]);//    * cep
+
+            Date dataNascimento = new SimpleDateFormat("dd/MM/yyyy").parse(dados[3]);
+
+            String[] telefones = dados[4].substring(dados[4].indexOf("[") + 1, dados[4].lastIndexOf("]")).split(",");
+
+            PessoaFisica p = new PessoaFisica(dados[1],// * nome
+                    dados[2],// * CPF
+                    dataNascimento,// * data de nascimento
+                    dados[5],// * email
+                    endereco,// * endereco
+                    telefones);
+
+            if (dados[7].equals(String.valueOf(false))) {
+                p.setCadastroAtivo(false);
+            }
+            return p;
+        } catch (DataBaseException | InvalidInputException | ParseException e) {
+            corrigeBanco();
+            throw new SystemErrorException("Os dados \"" + dadosCompletos + "\" foram corrompidos");
         }
-        return 0;// * objeto não encontrado
     }
 
     @Override
-    public String getNomeArquivoID() {
-        return PessoaFisica.getArquivoID();
-    }
-
-    @Override
-    public boolean isCadastroAtivo(PessoaFisica obj) {
-        return obj.isCadastroAtivo();
-    }
-
-    @Override
-    public PessoaFisica setCadastroAtivo(PessoaFisica obj, boolean flag) {
-        obj.setCadastroAtivo(flag);
-        return obj;
-    }
-
-    @Override
-    public int buscar(String dado) throws Exception {
+    public int buscar(String dado) throws InvalidInputException, SystemErrorException {
         ArrayList<PessoaFisica> listaPessoas = buscarTodos();
         for (PessoaFisica p : listaPessoas) {
             if (p.getCpf().equals(dado) && p.isCadastroAtivo()) {//    * encontrou
                 return getID(p);//  * retornando o id
             }
         }
-        return 0;// * objetoo não encontrado
+        return 0;// * objeto não encontrado
     }
 
     public int buscarPorNome(String Nome) throws Exception {
@@ -115,12 +93,17 @@ public class ManipulaBancoPessoaFisica implements IManipulaBanco<PessoaFisica> {
                 return getID(p);//  * retornando o id
             }
         }
-        return 0;// * objetoo não encontrado
+        return 0;// * objeto não encontrado
     }
 
     @Override
-    public boolean ativarEasterEgg(PessoaFisica obj) {
-        return obj.ativarEasterEgg();
+    public String getNomeArquivoDisco() {
+        return this.NomeArquivoDisco;
+    }
+
+    @Override
+    public int getQuantidadeDeDadosSalvos() {
+        return this.quantidadeDeDadosNoBanco;
     }
 
 }
